fcb01d56aef4b54e75b0c4b930a3ef44d55a5c72
Ivan1906 <yanik.ivan@ukr.net>
2018-05-13 19:10:29 +0300

    Add files

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const  createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const config = require('./config');
+const { MongoManager } = require('./src/mongo');
+const api = require('./src/api');
+const app = express();
+const mongoManager = new MongoManager(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+mongoManager.connect();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', api(config));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
