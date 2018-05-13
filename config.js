67ddd09184186f2796a1d61302b4a29b214aeb8b
Ivan1906 <yanik.ivan@ukr.net>
2018-05-13 21:31:11 +0300

    Add files

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_URL: 'mongodb://ask-apiko-user:ask-apiko-user-password@ds251807.mlab.com:51807/ask-apiko-api'
-}
+    MONGODB_URI: 'mongodb://ask-apiko-user:ask-apiko-user-password@ds251807.mlab.com:51807/ask-apiko-api'
+};  
